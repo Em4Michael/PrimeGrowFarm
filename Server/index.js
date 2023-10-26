100bf05cff6d1aa034f13e4b68da50ee99fd01aa
em4michael <aniehegc@gmail.com>
2023-10-26 14:12:44 -0400

    changed DB and Folder structure

---
+++ Server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const createWebSocketServer = require('./src/websocket/websocketServer');
+const sensorDataRoutes = require('./src/routes/sensorDataRoutes');
+
+require('dotenv').config();
+console.log(process.env.MONGODB_URI);
+
+const app = express();
+const server = app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+
+// WebSocket server
+createWebSocketServer(server);
+
+// Routes
+app.use('/api', sensorDataRoutes);
+app.use(express.static('public'));
